337302a2df9f3be74581300dec187e7ba2be0a3b
SandaminaWijenayake <125541926+SandaminaWijenayake@users.noreply.github.com>
2023-10-23 22:36:48 +0530

    fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,15 +10,17 @@ const Navbar = () => {
           <ul className="flex justify-around">
             <li>Packages</li>
             <li>Blogs</li>
-            <li>reviews</li>
+            <li>Reviews</li>
             <li>Contact us</li>
             <li>About us</li>
           </ul>
         </div>
         <div>Register now</div>
       </div>
-      <div className="absolute text-white  top-1/2 left-1/2 z-10 transform -translate-x-1/2 -translate-y-1/2 text-5xl font-sans  font-semibold   text-center">
-        JAGATH TRAVELS WARMLY WELCOME YOU TO THE PEARL OF THE INDIAN OCEAN
+      <div className="absolute text-white  top-1/2 w-9/12 left-1/2 z-10 transform -translate-x-1/2 -translate-y-1/2 text-5xl font-sans  font-semibold   text-center">
+        <p className="text-4xl mb-0">Sri Lanka</p> GENIUS LANKA TOURS HAS OPEN
+        DOORS TO WARMLY WELCOME YOU TO ITS COUNTLESS AND REMARKABLE EXPERIENCE
+        IN THE PEARL OF INDIAN OCEAN
       </div>
       <div className="relative">
         <Hero />
