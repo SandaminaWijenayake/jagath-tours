aafd547d4f47475e7bb3dc6f7fa0702699271467
SandaminaWijenayake <125541926+SandaminaWijenayake@users.noreply.github.com>
2023-10-29 19:48:00 +0530

    fix

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -9,71 +9,167 @@ const Packages = () => {
           Genius Lanka Tours packages
         </h1>
       </div>
-      <div className="w-3/5 m-auto">
+      <div className="w-3/4 m-auto">
         <h1 className="pb-5 font-semibold text-xl text-sky-700">
           Three Days (Two Nights Tour Packages)
         </h1>
         <table id="customers" className="">
           <tr>
+            <th className="w-14">No</th>
             <th>Tour name</th>
             <th>Theme</th>
             <th>Key Activities</th>
           </tr>
           <tr>
-            <td>
-              <ul>
-                <li>Anuradhapura</li>
-                <li>Polonnaruwa</li>
-                <li>Sigiriya</li>
-                <li>Dambulla</li>
-              </ul>
+            <td className="text-center text-sky-700 font-bold">1</td>
+            <td className="text-sm font-bold">
+              Anuradhapura + Polonnaruwa + Sigiriya & Dambulla
             </td>
             <td>
-              <ul>
+              <ul className="px-5 grid grid-cols-2 text-xs font-bold text-gray-700  gap-x-4 gap-2 list-disc">
                 <li>Culture</li>
                 <li>Ruins</li>
                 <li>Buddhism</li>
               </ul>
             </td>
             <td>
-              <ul>
+              <ul className="px-5 grid grid-cols-2 text-xs font-bold text-gray-700  gap-x-4 gap-2 list-disc">
                 <li>Explore the Ancient City/Ruins</li>
-                <li>Visit the Sri Maha Bodhi</li>
                 <li>Visit Gal Vihara</li>
                 <li>Climb Sigiriya Rock</li>
-                <li>Visit the Water Gardens</li>
               </ul>
             </td>
           </tr>
           <tr>
-            <td>Kandy + Dambulla + Sigiriya & Polonnaruwa</td>
-            <td></td>
-            <td></td>
+            <td className="text-center text-sky-700 font-bold">2</td>
+            <td className="text-sm font-bold">
+              Kandy + Dambulla + Sigiriya & Polonnaruwa
+            </td>
+            <td>
+              <ul className="px-5 grid grid-cols-2 text-xs font-bold text-gray-700  gap-x-4 gap-2 list-disc">
+                <li>Calture</li>
+                <li>histroy</li>
+                <li>Village Tours</li>
+                <li>Ancient Ruins</li>
+              </ul>
+            </td>
+            <td>
+              <ul className="px-5 grid grid-cols-2 text-xs font-bold text-gray-700  gap-x-4 gap-2 list-disc">
+                <li>Visit the Temple of the Tooth (Sri Dalada Maligawa)</li>
+                <li>Explore the Dambulla Cave Temple</li>
+                <li>Climb Sigiriya Rock</li>
+                <li>Tour the Ancient Ruins</li>
+              </ul>
+            </td>
           </tr>
           <tr>
-            <td>Yala + Nuwara -Eliya & Pinnawala</td>
-            <td></td>
-            <td></td>
+            <td className="text-center text-sky-700 font-bold">3</td>
+            <td className="text-sm font-bold">
+              Yala + Nuwara -Eliya & Pinnawala
+            </td>
+            <td>
+              <ul className="px-5 grid grid-cols-2 text-xs font-bold text-gray-700  gap-x-4 gap-2 list-disc">
+                <li>Safari</li>
+                <li>Camping</li>
+                <li>Tea</li>
+                <li>Elephant</li>
+              </ul>
+            </td>
+            <td>
+              <ul className="px-5 grid grid-cols-2 text-xs font-bold text-gray-700  gap-x-4 gap-2 list-disc">
+                <li>Yala National Park Safari</li>
+                <li>Tea Plantation Tours</li>
+                <li>Pinnawala Elephant Orphanage</li>
+              </ul>
+            </td>
           </tr>
           <tr>
-            <td>Galle + Yala + Nuwara Eliya & Kandy</td>
-            <td></td>
-            <td></td>
+            <td className="text-center text-sky-700 font-bold">4</td>
+            <td className="text-sm font-bold">
+              Galle + Yala + Nuwara Eliya & Kandy
+            </td>
+            <td>
+              <ul className="px-5 grid grid-cols-2 text-xs font-bold text-gray-700  gap-x-4 gap-2 list-disc">
+                <li>Historical City</li>
+                <li>Safari</li>
+                <li>Camping</li>
+                <li>Tea</li>
+                <li>Hiking</li>
+              </ul>
+            </td>
+            <td>
+              <ul className="px-5 grid grid-cols-2 text-xs font-bold text-gray-700  gap-x-4 gap-2 list-disc">
+                <li>Explore Galle Fort</li>
+                <li>Yala National Park Safari</li>
+                <li>Tea Plantation Tours</li>
+                <li>Temple of the Tooth (Sri Dalada Maligawa)</li>
+              </ul>
+            </td>
           </tr>
           <tr>
-            <td>Nuwara + Eliya-Worlds End & Udavalawa Safari</td>
-            <td></td>
-            <td></td>
+            <td className="text-center text-sky-700 font-bold">5</td>
+            <td className="text-sm font-bold">
+              Nuwara-Eliya + Worlds End & Udavalawa Safari
+            </td>
+            <td>
+              <ul className="px-5 grid grid-cols-2 text-xs font-bold text-gray-700  gap-x-4 gap-2 list-disc">
+                <li>Elephants</li>
+                <li>Hiking</li>
+                <li>Safari</li>
+              </ul>
+            </td>
+            <td>
+              <ul className="px-5 grid grid-cols-2 text-xs font-bold text-gray-700  gap-x-4 gap-2 list-disc">
+                <li>Hike to World's End</li>
+                <li>Explore Horton Plains</li>
+                <li>Tea Plantation Tours</li>
+                <li>Wildlife Safari</li>
+              </ul>
+            </td>
           </tr>
           <tr>
-            <td>Nuwara-Eliya & kandy</td>
-            <td></td>
-            <td></td>
+            <td className="text-center text-sky-700 font-bold">6</td>
+            <td className="text-sm font-bold">Nuwara-Eliya & kandy</td>
+            <td>
+              <ul className="px-5 grid grid-cols-2 text-xs font-bold text-gray-700  gap-x-4 gap-2 list-disc">
+                <li>Horse Riding</li>
+                <li>Tea</li>
+                <li>Cultural</li>
+              </ul>
+            </td>
+            <td>
+              <ul className="px-5 grid grid-cols-2 text-xs font-bold text-gray-700  gap-x-4 gap-2 list-disc">
+                <li>Visit a Tea Plantation</li>
+                <li>Hike to World's End</li>
+                <li>Gregory Lake</li>
+                <li>Temple of the Tooth</li>
+                <li>Peradeniya Botanical Gardens</li>
+              </ul>
+            </td>
           </tr>
           <tr>
-            <td>Tirncomali + Sigiriya & Dambulla</td>
-            <td></td>
-            <td></td>
+            <td className="text-center text-sky-700 font-bold">7</td>
+            <td className="text-sm font-bold">
+              Trincomali + Sigiriya & Dambulla
+            </td>
+            <td>
+              <ul className="px-5 grid grid-cols-2 text-xs font-bold text-gray-700  gap-x-4 gap-2 list-disc">
+                <li>Diving</li>
+                <li>Hot Springs</li>
+                <li>Tamil Culture</li>
+                <li>Village Life</li>
+              </ul>
+            </td>
+            <td>
+              <ul className="px-5 grid grid-cols-2 text-xs font-bold text-gray-700  gap-x-4 gap-2 list-disc">
+                <li>Visit Trincomalee Beaches</li>
+                <li>Trincomalee Harbour</li>
+                <li>Hot Springs</li>
+                <li>Climb Sigiriya Rock</li>
+                <li>Village Tours</li>
+                <li>Dambulla Cave Temple</li>
+              </ul>
+            </td>
           </tr>
         </table>
       </div>
