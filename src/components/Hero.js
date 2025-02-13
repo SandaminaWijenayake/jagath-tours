d35e16a3cdb60d0cc2a640a20d7e60cc53c019e5
SandaminaWijenayake <sandaminawijenayake0717@gmail.com>
2025-02-14 05:27:47 +0530

    vsvs

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import image from "../images/beach-3.jpg";
 import image1 from "../images/photosgeniuslankatours/kandy/the-temple-of-tooth.jpg";
-import test1 from "../images/photosgeniuslankatours/slider-4-slide-1-1920x678.jpg";
-import test2 from "../images/photosgeniuslankatours/slider-4-slide-2-1920x678.jpg";
-import test3 from "../images/photosgeniuslankatours/slider-4-slide-3-1920x678.jpg";
+import test1 from "../images/photosgeniuslankatours/kandy/Kandy-and-The-Temple.jpg";
+import test2 from "../images/beach-3.jpg";
+import test3 from "../images/photosgeniuslankatours/148.jpg";
 import { color, motion } from "framer-motion";
 import { Link, useNavigate } from "react-router-dom";
 
